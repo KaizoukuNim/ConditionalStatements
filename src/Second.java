2a3cab20814b14e16681ab5ec9de0aaeacc60d89
Nimesh Rai <146310164+KaizoukuNim@users.noreply.github.com>
2024-07-16 13:44:31 +0545

    nimesh1

--- src/Second.java
+++ src/Second.java
@@ -1,5 +1,16 @@
+import java.util.Scanner;
 public class Second {
     public static void main(String[] args) {
+  int user_age;
+ Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your age");
+        user_age = sc.nextInt();
 
+        if (user_age>=18){
+            System.out.println("You are eligible to vote");
+        }
+        else {
+            System.out.println("You are not eligible to vote");
+        }
     }
 }
